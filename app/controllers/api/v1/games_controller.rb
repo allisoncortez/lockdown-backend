89f1be907d2b6d3eabc1b4f69908a842707ad384
allisoncortez <allizetorc@gmail.com>
2020-06-02 17:28:47 -0500

    game controller create route added

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -4,4 +4,14 @@ class Api::V1::GamesController < ApplicationController
         render json: games
     end
 
+    def create
+        player = Player.find_or_create_by(name: params[:player_name])
+        game = Game.new(player: player, score: params[:score])
+        if game.save
+            render json: game, include: [:player], status:200
+        else
+            render json: {errors: game.errors.full_messages}, status: :unprocessible_entity
+        end
+    end
+
 end
